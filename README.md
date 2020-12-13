e309b9eea5c8d9ad9979dd70b399d350c0c61aab
AleksaC <aleksacukovic1@gmail.com>
2020-12-13 18:15:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# hadolint-pre-commit
+
+[![license](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE)
+
+A python package that provides a pip-installable
+[hadolint](https://github.com/hadolint/hadolint) binary.
+
+## Getting started
+
+### Installation
+
+The package hasn't been published to PyPI yet, and may never be, as its primary
+purpose doesn't require it. However you can install it through git:
+
+```shell script
+pip install git+git://github.com/AleksaC/hadolint-py.git@v1.19.0
+```
+
+To install another version simply replace the v1.19.0 with the version you want.
+
+### With pre-commit
+
+This package was primarily built to provide a convenient way of running hadolint
+as a [pre-commit](https://pre-commit.com) hook, since haskell isn't supported by
+pre-commit. An alternative to this solution is to create a docker hook since
+hadolint provides a docker image, but I think that it has unnecessary amount of overhead.
+
+Example `.pre-commit-config.yaml` with rules `DL3025` and `DL3018` excluded:
+
+```yaml
+repos:
+  - repo: https://github.com/AleksaC/hadolint-py
+    rev: v1.19.0
+    hooks:
+      - id: hadolint
+        args: [--ignore, DL3025, --ignore, DL3018]
+```
+
+## Contact 🙋‍♂️
+
+-   [Personal website](https://aleksac.me)
+-   <a target="_blank" href="http://twitter.com/aleksa_c_"><img alt='Twitter followers' src="https://img.shields.io/twitter/follow/aleksa_c_.svg?style=social"></a>
+-   aleksacukovic1@gmail.com
---
+++ setup.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import http
+import os.path
+import stat
+import sys
+import urllib.request
+from distutils.command.build import build as orig_build
+from distutils.core import Command
+
+from setuptools import setup
+from setuptools.command.install import install as orig_install
+
+HADOLINT_VERSION = "1.19.0"
+BASE_URL = "https://github.com/hadolint/hadolint/releases/download/v{}/hadolint-{}"
+POSTFIX = {
+    "linux": "Linux-x86_64",
+    "darwin": "Darwin-x86_64",
+    "win32": "Windows-x86_64.exe",
+}
+PY_VERSION = "1"
+
+
+def get_download_url() -> str:
+    return BASE_URL.format(HADOLINT_VERSION, POSTFIX[sys.platform])
+
+
+def download(url: str) -> bytes:
+    with urllib.request.urlopen(url) as resp:
+        code = resp.getcode()
+        if code != http.HTTPStatus.OK:
+            raise ValueError(f"HTTP failure. Code: {code}")
+        data = resp.read()
+
+    return data
+
+
+def save_executable(data: bytes, base_dir: str):
+    exe = "hadolint" if sys.platform != "win32" else "hadolint.exe"
+    output_path = os.path.join(base_dir, exe)
+    os.makedirs(base_dir)
+
+    with open(output_path, "wb") as fp:
+        fp.write(data)
+
+    # Mark as executable.
+    # https://stackoverflow.com/a/14105527
+    mode = os.stat(output_path).st_mode
+    mode |= stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+    os.chmod(output_path, mode)
+
+
+class build(orig_build):
+    sub_commands = orig_build.sub_commands + [("fetch_binaries", None)]
+
+
+class install(orig_install):
+    sub_commands = orig_install.sub_commands + [("install_shellcheck", None)]
+
+
+class fetch_binaries(Command):
+    build_temp = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        self.set_undefined_options("build", ("build_temp", "build_temp"))
+
+    def run(self):
+        # save binary to self.build_temp
+        url = get_download_url()
+        data = download(url)
+        save_executable(data, self.build_temp)
+
+
+class install_shellcheck(Command):
+    description = "install the shellcheck executable"
+    outfiles = ()
+    build_dir = install_dir = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        # this initializes attributes based on other commands' attributes
+        self.set_undefined_options("build", ("build_temp", "build_dir"))
+        self.set_undefined_options(
+            "install",
+            ("install_scripts", "install_dir"),
+        )
+
+    def run(self):
+        self.outfiles = self.copy_tree(self.build_dir, self.install_dir)
+
+    def get_outputs(self):
+        return self.outfiles
+
+
+command_overrides = {
+    "install": install,
+    "install_shellcheck": install_shellcheck,
+    "build": build,
+    "fetch_binaries": fetch_binaries,
+}
+
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as orig_bdist_wheel
+except ImportError:
+    pass
+else:
+    class bdist_wheel(orig_bdist_wheel):
+        def finalize_options(self):
+            orig_bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            _, _, plat = orig_bdist_wheel.get_tag(self)
+            # We don't contain any python source, nor any python extensions
+            return "py2.py3", "none", plat
+
+    command_overrides["bdist_wheel"] = bdist_wheel
+
+setup(version=f"{HADOLINT_VERSION}.{PY_VERSION}", cmdclass=command_overrides)
